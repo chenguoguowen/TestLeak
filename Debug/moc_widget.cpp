1ff65f7fa337ea637d53847668c2cf4f87e8d63b
chenguoguowen <chenguoguowen@163.com>
2017-04-21 14:46:15 +0800

    1

--- Debug/moc_widget.cpp
+++ Debug/moc_widget.cpp
@@ -19,8 +19,8 @@
 
 QT_BEGIN_MOC_NAMESPACE
 struct qt_meta_stringdata_Widget_t {
-    QByteArrayData data[4];
-    char stringdata0[39];
+    QByteArrayData data[8];
+    char stringdata0[88];
 };
 #define QT_MOC_LITERAL(idx, ofs, len) \
     Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
@@ -30,12 +30,17 @@ struct qt_meta_stringdata_Widget_t {
 static const qt_meta_stringdata_Widget_t qt_meta_stringdata_Widget = {
     {
 QT_MOC_LITERAL(0, 0, 6), // "Widget"
-QT_MOC_LITERAL(1, 7, 21), // "on_pushButton_clicked"
-QT_MOC_LITERAL(2, 29, 0), // ""
-QT_MOC_LITERAL(3, 30, 8) // "updateAA"
+QT_MOC_LITERAL(1, 7, 8), // "recvData"
+QT_MOC_LITERAL(2, 16, 0), // ""
+QT_MOC_LITERAL(3, 17, 10), // "onProgress"
+QT_MOC_LITERAL(4, 28, 9), // "connected"
+QT_MOC_LITERAL(5, 38, 12), // "disconnected"
+QT_MOC_LITERAL(6, 51, 7), // "onError"
+QT_MOC_LITERAL(7, 59, 28) // "QAbstractSocket::SocketError"
 
     },
-    "Widget\0on_pushButton_clicked\0\0updateAA"
+    "Widget\0recvData\0\0onProgress\0connected\0"
+    "disconnected\0onError\0QAbstractSocket::SocketError"
 };
 #undef QT_MOC_LITERAL
 
@@ -45,7 +50,7 @@ static const uint qt_meta_data_Widget[] = {
        7,       // revision
        0,       // classname
        0,    0, // classinfo
-       2,   14, // methods
+       5,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
@@ -53,12 +58,18 @@ static const uint qt_meta_data_Widget[] = {
        0,       // signalCount
 
  // slots: name, argc, parameters, tag, flags
-       1,    0,   24,    2, 0x0a /* Public */,
-       3,    0,   25,    2, 0x0a /* Public */,
+       1,    1,   39,    2, 0x0a /* Public */,
+       3,    0,   42,    2, 0x0a /* Public */,
+       4,    0,   43,    2, 0x0a /* Public */,
+       5,    0,   44,    2, 0x0a /* Public */,
+       6,    1,   45,    2, 0x0a /* Public */,
 
  // slots: parameters
+    QMetaType::Void, QMetaType::QStringList,    2,
     QMetaType::Void,
     QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void, 0x80000000 | 7,    2,
 
        0        // eod
 };
@@ -69,12 +80,25 @@ void Widget::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void
         Widget *_t = static_cast<Widget *>(_o);
         Q_UNUSED(_t)
         switch (_id) {
-        case 0: _t->on_pushButton_clicked(); break;
-        case 1: _t->updateAA(); break;
+        case 0: _t->recvData((*reinterpret_cast< QStringList(*)>(_a[1]))); break;
+        case 1: _t->onProgress(); break;
+        case 2: _t->connected(); break;
+        case 3: _t->disconnected(); break;
+        case 4: _t->onError((*reinterpret_cast< QAbstractSocket::SocketError(*)>(_a[1]))); break;
         default: ;
         }
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        switch (_id) {
+        default: *reinterpret_cast<int*>(_a[0]) = -1; break;
+        case 4:
+            switch (*reinterpret_cast<int*>(_a[1])) {
+            default: *reinterpret_cast<int*>(_a[0]) = -1; break;
+            case 0:
+                *reinterpret_cast<int*>(_a[0]) = qRegisterMetaType< QAbstractSocket::SocketError >(); break;
+            }
+            break;
+        }
     }
-    Q_UNUSED(_a);
 }
 
 const QMetaObject Widget::staticMetaObject = {
@@ -102,13 +126,13 @@ int Widget::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
     if (_id < 0)
         return _id;
     if (_c == QMetaObject::InvokeMetaMethod) {
-        if (_id < 2)
+        if (_id < 5)
             qt_static_metacall(this, _c, _id, _a);
-        _id -= 2;
+        _id -= 5;
     } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
-        if (_id < 2)
-            *reinterpret_cast<int*>(_a[0]) = -1;
-        _id -= 2;
+        if (_id < 5)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 5;
     }
     return _id;
 }
